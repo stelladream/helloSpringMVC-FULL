d2e69d3ac4cb50b7aaacaba54eca911521103ddc
stelladream <neonykim@gmail.com>
2025-04-18 19:23:10 +0900

    Update DataInitializer.java

--- src/main/java/kr/ac/hansung/cse/controller/DataInitializer.java
+++ src/main/java/kr/ac/hansung/cse/controller/DataInitializer.java
@@ -1,25 +1,27 @@
 package kr.ac.hansung.cse.controller;
 
+import jakarta.annotation.PostConstruct;
 import kr.ac.hansung.cse.dao.OfferDao;
 import kr.ac.hansung.cse.model.Offer;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+// vm optons: -Dspring.profiles.active=dev,
+// dev: 개발 환경
+//test: 테스트 환경
+//prod: 운영 환경
+@Profile("dev")
 @Component
-public class DataInitializer implements InitializingBean {
+public class DataInitializer {
 
     @Autowired
     private OfferDao offerDao;
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        Offer offer1 = new Offer("Alice","alice@hansung.ac.kr","스프링이 참 재미있네요");
-        Offer offer2 = new Offer("Bob","bob@hansung.ac.kr","JPA/Hibernate는 참 편리하네요");
-        Offer offer3 = new Offer("Charlie","charlie@hansung.ac.kr","Rest API를 구현합니다.");
-
-        offerDao.insert(offer1);
-        offerDao.insert(offer2);
-        offerDao.insert(offer3);
+    @PostConstruct
+    public void init() {
+        offerDao.insert(new Offer("Alice", "alice@hansung.ac.kr", "스프링이 참 재미있네요"));
+        offerDao.insert(new Offer("Bob", "bob@hansung.ac.kr", "JPA/Hibernate는 참 편리하네요"));
+        offerDao.insert(new Offer("Charlie", "charlie@hansung.ac.kr", "Rest API를 구현합니다."));
     }
 }
